84affbf381d10b726ced7189ff86129f691e0abe
dalu-zhanglu <535661173@qq.com>
2018-07-03 23:50:31 +0800

    baseurl

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@
 
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL:'"http://stone.yeager.vip/wst-boss"' //测试环境
-  //BASE_URL:'"http://test.yeager.vip/wst-boss"' //部署生产环境时候用这个
+  //BASE_URL:'"http://stone.yeager.vip/wst-boss"' //部署测试环境时候用这个
+  BASE_URL:'"http://test.yeager.vip/wst-boss"' //部署生产环境时候用这个
 }
