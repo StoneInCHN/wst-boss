e47ce1800a7d070cef82482b3b8738371461c880
tanbiao <tanbiao_cd@keruyun.com>
2018-07-19 20:34:15 +0800

    fix bug

--- src/pages/mine/Mine.vue
+++ src/pages/mine/Mine.vue
@@ -1,7 +1,34 @@
 <template>
-	<div>
-		<Header backUrl="/"/>
-		<h2 class="sub_title">我的</h2>
+	<div class="mine">
+		<div class="statistical">
+			<h2 class="title">收支统计</h2>
+			<div class="statistical-item">
+				<div class="statistical-sub-item">
+					<div >
+						<p class="sub-title">本月收入</p>
+						<p>15077.00</p>
+					</div>
+					<div>
+						<p class="sub-title">今日收入</p>
+						<p>1022.00</p>
+					</div>
+				</div>
+				<p>累计收入：140532.00</p>
+			</div>
+			<div class="statistical-item">
+				<div class="statistical-sub-item">
+					<div>
+						<p class="sub-title">本月支出</p>
+						<p>3377.00</p>
+					</div>
+					<div>
+						<p class="sub-title">进入支出</p>
+						<p>122.00</p>
+					</div>
+				</div>
+				<p>累计支出：20490.00</p>
+			</div>
+		</div>
 		<CellGroup>
 			<Cell title="店铺管理" icon="shop" is-link to="/manage/storeManage" />
 			<Cell title="员工管理" icon="shop" is-link to="/manage/workerManage" />
@@ -12,29 +39,48 @@
 </template>
 
 <script>
-import { Cell, CellGroup, Icon } from 'vant'
-import Header from "../wechat/Header"
-import Footer from "../wechat/Footer"
-export default{
-	name: "Mine",
-	components: { Header, Footer, Cell, CellGroup, Icon },
-	data () {
-		return {
-
-		}
-	}
-}
+import { Cell, CellGroup, Icon } from "vant";
+import Header from "../wechat/Header";
+import Footer from "../wechat/Footer";
+export default {
+  name: "Mine",
+  components: { Header, Footer, Cell, CellGroup, Icon },
+  data() {
+    return {};
+  }
+};
 </script>
 
-<style scoped>
-	.right_icon{
-		float:right;
-	}
-	.sub_title {
-	    margin: 0;
-	    font-weight: 400;
-	    font-size: 14px;
-	    color: rgba(69,90,100,.6);
-	    padding: 20px 15px 10px;
-	}
+<style lang="less">
+.mine {
+  .statistical {
+    .title {
+		text-align: center;
+    }
+    .sub_title {
+      margin: 0;
+      font-weight: 400;
+      font-size: 14px;
+      color: rgba(69, 90, 100, 0.6);
+      padding: 20px 15px 10px;
+    }
+    &-item {
+      .statistical-sub-item {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        & > div {
+          flex: 1 1 auto;
+          text-align: center;
+        }
+        & > div:last-child {
+          border-left: 1px solid #333;
+        }
+      }
+      & > p {
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
