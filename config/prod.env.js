79e523ce6b263ca3eda5331df37f90af3279e1ad
tanbiao <tanbiao_cd@keruyun.com>
2019-01-04 17:49:55 +0800

    fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,5 @@
-//"http://47.106.96.230:10002/wst-boss/";//生产环境 IP
-//"http://test.yeager.vip/wst-boss/";//生产环境 域名
 
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL:'"http://www.yunshuizhan.cn:20001/wst-boss"' //部署测试环境时候用这个
-  //BASE_URL:'"http://test.yeager.vip/wst-boss"' //部署生产环境时候用这个
+  BASE_URL:'"http://www.yunshuizhan.cn:20002/wst-boss"' 
 }
